7dd93744da4f62f38f3e02bc680d696ccddaf2ae
Wong Hoi Sing Edison <hswong3i@gmail.com>
2013-05-06 01:35:55 +0800

    Add ResponseType skeleton and test cases.

--- ResponseType/CodeResponseType.php
+++ ResponseType/CodeResponseType.php
@@ -14,12 +14,113 @@ namespace Pantarei\Oauth2\ResponseType;
 /**
  * Code response type implementation.
  *
+ * @see http://tools.ietf.org/html/rfc6749#section-4.1
+ *
  * @author Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
  */
 class CodeResponseType implements ResponseTypeInterface
 {
+  /**
+   * REQUIRED. Value MUST be set to "code".
+   *
+   * @see http://tools.ietf.org/html/rfc6749#section-4.1.1
+   */
+  private $responseType = 'code';
+
+  /**
+   * REQUIRED. The client identifier as described in Section 2.2.
+   *
+   * @see http://tools.ietf.org/html/rfc6749#section-4.1.1
+   */
+  private $clientId = '';
+
+  /**
+   * OPTIONAL. As described in Section 3.1.2.
+   *
+   * @see http://tools.ietf.org/html/rfc6749#section-4.1.1
+   */
+  private $redirectUri = '';
+
+  /**
+   * OPTIONAL. The scope of the access request as described by Section 3.3.
+   *
+   * @see http://tools.ietf.org/html/rfc6749#section-4.1.1
+   */
+  private $scope = '';
+
+  /**
+   * RECOMMENDED. An opaque value used by the client to maintain
+   * state between the request and callback. The authorization
+   * server includes this value when redirecting the user-agent back
+   * to the client. The parameter SHOULD be used for preventing
+   * cross-site request forgery as described in Section 10.12.
+   *
+   * @see http://tools.ietf.org/html/rfc6749#section-4.1.1
+   */
+  private $state = '';
+
+  public function __construct(array $query = array())
+  {
+    if (isset($query['client_id'])) {
+      $this->setClientId($query['client_id']);
+    }
+    if (isset($query['redirect_uri'])) {
+      $this->setRedirectUri($query['redirect_uri']);
+    }
+    if (isset($query['scope'])) {
+      $this->setScope($query['scope']);
+    }
+    if (isset($query['state'])) {
+      $this->setState($query['state']);
+    }
+  }
+
   public function getResponseType()
   {
-    return 'code';
+    return $this->responseType;
+  }
+
+  public function setClientId($clientId)
+  {
+    $this->clientId = $clientId;
+    return $this;
+  }
+
+  public function getClientId()
+  {
+    return $this->clientId;
+  }
+
+  public function setRedirectUri($redirectUri)
+  {
+    $this->redirectUri = $redirectUri;
+    return $this;
+  }
+
+  public function getRedirectUri()
+  {
+    return $this->redirectUri;
+  }
+
+  public function setScope($scope)
+  {
+    $this->scope = $scope;
+    return $this;
+  }
+
+  public function getScope()
+  {
+    return $this->scope;
+  }
+
+  public function setState($state)
+  {
+    $this->state = $state;
+    return $this;
+  }
+
+  public function getState()
+  {
+    return $this->state;
   }
 }
--- Tests/ResponseType/CodeResponseTypeTest.php
+++ Tests/ResponseType/CodeResponseTypeTest.php
@@ -20,9 +20,59 @@ use Pantarei\Oauth2\ResponseType\CodeResponseType;
  */
 class CodeResponseTypeTest extends \PHPUnit_Framework_TestCase
 {
-  public function testGetResponseType()
+  public function testResponseType()
   {
-    $grant_type = new CodeResponseType();
-    $this->assertEquals('code', $grant_type->getResponseType());
+    $response_type = new CodeResponseType();
+    $this->assertEquals('code', $response_type->getResponseType());
+  }
+
+  public function testClientId()
+  {
+    $response_type = new CodeResponseType(array(
+      'client_id' => '1234',
+    ));
+    $this->assertEquals('1234', $response_type->getClientId());
+
+    $response_type->setClientId('5678');
+    $this->assertEquals('5678', $response_type->getClientId());
+  }
+
+  public function testRedirectUri()
+  {
+    $response_type = new CodeResponseType(array(
+      'client_id' => '1234',
+      'redirect_uri' => 'http://example.com/redirect',
+    ));
+    $this->assertEquals('http://example.com/redirect', $response_type->getRedirectUri());
+
+    $response_type->setRedirectUri('http://abc.com/redirect');
+    $this->assertEquals('http://abc.com/redirect', $response_type->getRedirectUri());
+  }
+
+  public function testScope()
+  {
+    $response_type = new CodeResponseType(array(
+      'client_id' => '1234',
+      'redirect_uri' => 'http://example.com/redirect',
+      'scope' => 'aaa bbb ccc',
+    ));
+    $this->assertEquals('aaa bbb ccc', $response_type->getScope());
+
+    $response_type->setScope('ddd eee fff');
+    $this->assertEquals('ddd eee fff', $response_type->getScope());
+  }
+
+  public function testState()
+  {
+    $response_type = new CodeResponseType(array(
+      'client_id' => '1234',
+      'redirect_uri' => 'http://example.com/redirect',
+      'scope' => 'aaa bbb ccc',
+      'state' => 'demo state',
+    ));
+    $this->assertEquals('demo state', $response_type->getState());
+
+    $response_type->setState('example state');
+    $this->assertEquals('example state', $response_type->getState());
   }
 }
