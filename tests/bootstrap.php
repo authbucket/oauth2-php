2278ffb470b41a5e2ae534296ecb9799bf331a74
Wong Hoi Sing Edison <hswong3i@gmail.com>
2013-05-15 15:25:17 +0800

    Revamp folder structure, reference https://github.com/composer/composer.

---
+++ tests/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * This file is part of the pantarei/oauth2 package.
+ *
+ * (c) Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+$loader = require __DIR__ . '/../vendor/autoload.php';
+$loader->add('Pantarei\\OAuth2\\Tests', __DIR__);
