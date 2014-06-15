dd9dc337dc00f4a31c31ecb07467244cd11ff299
Wong Hoi Sing Edison <hswong3i@gmail.com>
2014-06-15 18:47:29 +0800

    Remove prod/test setup, we only use --env=dev for testing.

--- app/config/config_dev.php
+++ app/config/config_dev.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-require __DIR__ . '/config_prod.php';
+require __DIR__ . '/config.php';
 
 $app['db.options'] = array(
     'driver' => 'pdo_sqlite',
