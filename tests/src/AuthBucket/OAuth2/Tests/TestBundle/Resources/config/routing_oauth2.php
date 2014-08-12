dd162218220ef4183615b28291849753b934a787
Wong Hoi Sing Edison <hswong3i@gmail.com>
2014-08-12 12:30:23 +0800

    Update as CRUDL style.

--- tests/src/AuthBucket/OAuth2/Tests/TestBundle/Resources/config/routing_oauth2.php
+++ tests/src/AuthBucket/OAuth2/Tests/TestBundle/Resources/config/routing_oauth2.php
@@ -28,16 +28,21 @@ $app->match('/oauth2/debug', 'authbucket_oauth2.debug_controller:debugAction')
     ->bind('oauth2_debug');
 
 $app->post('/oauth2/model/{type}.{_format}', 'authbucket_oauth2.model_controller:createModelAction')
+    ->bind('oauth2_model_type_create')
     ->assert('_format', 'json|xml');
 
 $app->get('/oauth2/model/{type}/{id}.{_format}', 'authbucket_oauth2.model_controller:readModelAction')
-    ->assert('_format', 'json|xml');
-
-$app->get('/oauth2/model/{type}.{_format}', 'authbucket_oauth2.model_controller:readModelAllAction')
+    ->bind('oauth2_model_type_read')
     ->assert('_format', 'json|xml');
 
 $app->put('/oauth2/model/{type}/{id}.{_format}', 'authbucket_oauth2.model_controller:updateModelAction')
+    ->bind('oauth2_model_type_update')
     ->assert('_format', 'json|xml');
 
 $app->delete('/oauth2/model/{type}/{id}.{_format}', 'authbucket_oauth2.model_controller:deleteModelAction')
+    ->bind('oauth2_model_type_delete')
+    ->assert('_format', 'json|xml');
+
+$app->get('/oauth2/model/{type}.{_format}', 'authbucket_oauth2.model_controller:listModelAction')
+    ->bind('oauth2_model_type_list')
     ->assert('_format', 'json|xml');
