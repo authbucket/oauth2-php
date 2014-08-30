454b03cc027253852966666d4fc5e52d15760754
Wong Hoi Sing Edison <hswong3i@gmail.com>
2014-08-30 17:48:08 +0800

    Rename project as authbucket/oauth2-php.

--- app/config/routing_test.php
+++ app/config/routing_test.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This file is part of the authbucket/oauth2 package.
+ * This file is part of the authbucket/oauth2-php package.
  *
  * (c) Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
  *
