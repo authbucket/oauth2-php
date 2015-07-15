5d4f61c21e67e8b5a49d8c95b235d9d4619dce84
Wong Hoi Sing Edison <hswong3i@gmail.com>
2015-07-15 19:36:30 +0800

    Sync bootstrap_test.php with oauth2-symfony-bundle.

--- app/bootstrap_test.php
+++ app/bootstrap_test.php
@@ -6,4 +6,5 @@ passthru(__DIR__.'/console --env=test -q doctrine:schema:drop --force');
 passthru(__DIR__.'/console --env=test -q doctrine:schema:create');
 passthru(__DIR__.'/console --env=test -q doctrine:fixtures:load -n');
 
-require __DIR__.'/bootstrap.php';
+$loader = require __DIR__.'/bootstrap.php';
+$loader->add('AuthBucket\OAuth2\Tests', __DIR__.'../tests');
