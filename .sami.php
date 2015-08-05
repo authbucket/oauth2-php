3a8d724ad2863fce52ef5bd8ea765543c75d5130
Wong Hoi Sing Edison <hswong3i@gmail.com>
2015-08-05 14:30:03 +0800

    Update Sami theme as default.

--- .sami.php
+++ .sami.php
@@ -16,7 +16,7 @@ $versions = GitVersionCollection::create($dir)
     ->addFromTags('*');
 
 return new Sami($iterator, array(
-    'theme' => 'enhanced',
+    'theme' => 'default',
     'versions' => $versions,
     'title' => 'AuthBucket\OAuth2 API',
     'build_dir' => __DIR__ . '/build/sami/%version%',
